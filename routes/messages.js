7eca0db8dcf86225114bf66b1d780f9fabdb8041
SugeydiMagno <l19350303@tuxtepec.tecnm.mx>
2022-09-28 11:21:56 -0500

    ARCHIVOS

--- routes/messages.js
+++ routes/messages.js
@@ -1,6 +1,19 @@
-const {Router} = require("express")
+const {Router, Route} = require("express")
 const router = Router ()
+const {rootMessage,
+       hiMessage,
+       byeMessage,
+       postMessage,
+       putMessage,
+       deletMessage
+    }= require('../controllers/messages')
 
-router.get("/",(req, res) => {res.send("Holaa!")})
+router.get("/", rootMessage) //End Point
+router.get("/hi", hiMessage) //End Point
+router.get("/bye", byeMessage) //End Point
+
+router.post('/',putMessage)
+router.post('/',postMessage)
+router.post('/',deletMessage)
 
 module.exports = router
